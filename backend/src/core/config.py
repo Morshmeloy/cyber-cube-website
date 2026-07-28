618d05e310e10e4077f096f3ce4173137ef51ee8
Egor2007 <egorbanny2@gmail.com>
2026-07-28 06:39:06 +0300

    Добавлены константы для одинес

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -12,9 +12,12 @@ class Settings(BaseSettings):
     APP_NAME: str = Field("D4 Technologies", env="APP_NAME")
     DEBUG: bool = Field(False, env="DEBUG")
     CORS_ALLOWED_ORIGINS: list[str] = Field(["*"], env="CORS_ALLOWED_ORIGINS")
+    ONEC_BASE_URL: str = Field(..., env="ONEC_BASE_URL")
+    ONEC_USERNAME: str = Field(..., env="ONEC_USERNAME")
+    ONEC_PASSWORD: str = Field(..., env="ONEC_PASSWORD")
 
     class Config:
-        env_file = ".env"
+        env_file = ".env.dev"
         env_file_encoding = "utf-8"
 
 
