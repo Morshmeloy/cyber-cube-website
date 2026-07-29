55e8689c3e3ced19caa0e58e1e28d03f94952af6
Egor2007 <egorbanny2@gmail.com>
2026-07-29 23:40:24 +0300

    Добавлено поле для пометки что отправлено на одинес

--- backend/src/models/warehouse.py
+++ backend/src/models/warehouse.py
@@ -46,6 +46,7 @@ class StockOperation(Base):
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     exported_at = Column(DateTime(timezone=True), nullable=True)
+    confirmed_in_1c_at = Column(DateTime(timezone=True), nullable=True)
 
     nomenclature = relationship("Nomenclature", back_populates="operations")
     user = relationship("User", back_populates="stock_operations")
