f5193441cd50767b92801c0d700426609c11a04b
Egor2007 <egorbanny2@gmail.com>
2026-08-05 09:36:12 +0300

    Изменено в схеме для создание пользователей поле для ролей. Теперь там id. Также создана схема для обновления пользователя. Изменен в ответе пользователя ответ для роли, теперь там RoleResponse

--- backend/src/schemas/auth.py
+++ backend/src/schemas/auth.py
@@ -1,21 +1,26 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
-from src.models.user import UserRole
+from src.schemas.role import RoleResponse
 
 
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
-    role: UserRole = UserRole.ACCOUNTANT
+    role_id: int
     full_name: Optional[str] = None
+
+
+class UserUpdate(BaseModel):
+    role_id: Optional[int] = None
+    is_active: Optional[bool] = None
 
 
 class UserResponse(BaseModel):
     id: int
     username: str
     email: str
-    role: UserRole
+    role: RoleResponse
     full_name: Optional[str]
     is_active: bool
 
